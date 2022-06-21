995dbfbbe88e2e5a47ee8b17cb204a070de8daeb
Linh Vu <linh777gkog@gmail.com>
2022-06-21 22:52:26 +0700

    commit

--- public/app.js
+++ public/app.js
@@ -107,3 +107,57 @@ document.addEventListener('DOMContentLoaded', () => {
         setupButtons.style.display = 'none'
       }
     })
+
+    
+    // Check player status
+    socket.on('check-players', players => {
+      players.forEach((p, i) => {
+        if(p.connected) playerConnectedOrDisconnected(i)
+        if(p.ready) {
+          playerReady(i)
+          if(i !== playerReady) enemyReady = true
+        }
+      })
+    })
+
+    // On Timeout
+    socket.on('timeout', () => {
+      infoDisplay.innerHTML = 'You have reached the 10 minute limit'
+    })
+
+    // Ready button click
+    startButton.addEventListener('click', () => {
+      if(allShipsPlaced) playGameMulti(socket)
+      else infoDisplay.innerHTML = "Please place all ships"
+    })
+
+    // Setup event listeners for firing
+    computerSquares.forEach(square => {
+      square.addEventListener('click', () => {
+        if(currentPlayer === 'user' && ready && enemyReady) {
+          shotFired = square.dataset.id
+          socket.emit('fire', shotFired)
+        }
+      })
+    })
+
+    // On Fire Received
+    socket.on('fire', id => {
+      enemyGo(id)
+      const square = userSquares[id]
+      socket.emit('fire-reply', square.classList)
+      playGameMulti(socket)
+    })
+
+    // On Fire Reply Received
+    socket.on('fire-reply', classList => {
+      revealSquare(classList)
+      playGameMulti(socket)
+    })
+
+    function playerConnectedOrDisconnected(num) {
+      let player = `.p${parseInt(num) + 1}`
+      document.querySelector(`${player} .connected`).classList.toggle('active')
+      if(parseInt(num) === playerNum) document.querySelector(player).style.fontWeight = 'bold'
+    }
+  }
